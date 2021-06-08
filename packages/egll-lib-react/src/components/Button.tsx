5a852eb981e235a65737c7fe9bbf11ccb80541e1
Eduardo Guzman Lau Len <eduardo.laulen@nzxt.com>
2021-06-07 23:33:48 -0400

    feat: add monorepo initial commit

---
+++ packages/egll-lib-react/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const PrimaryButton = styled.button`
+  width: 200px;
+  height: 70px;
+  background-color: #560bad;
+  border: 1px solid #3a0ca3;
+  font-size: 1.5rem;
+  color: white;
+  :hover {
+    border-color: #4cc9f0;
+    background-color: #3f37c9;
+  }
+`
