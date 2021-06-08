39ba7e938ebbe83d556cac8cce9fbbe6aae38247
Eduardo Guzman Lau Len <eduardo.laulen@nzxt.com>
2021-06-08 00:33:35 -0400

    feat: fix TS lint and format

--- packages/egll-lib-react/src/components/Button.tsx
+++ packages/egll-lib-react/src/components/Button.tsx
@@ -1,28 +1,28 @@
 import styled from '../lib/styled';
 import React from 'react';
 import { useTheme } from 'emotion-theming';
-import {Theme} from '../themes';
+import { Theme } from '../themes';
 
 const BaseButton = styled.button`
   width: 200px;
   height: 70px;
-  background-color: ${({theme}) => theme.base.background};
-  border: 1px solid ${({theme}) => theme.base.border};
+  background-color: ${({ theme }) => theme.base.background};
+  border: 1px solid ${({ theme }) => theme.base.border};
   font-size: 1.5rem;
-  color: ${({theme}) => theme.base.color};
+  color: ${({ theme }) => theme.base.color};
   :hover {
-    border-color: ${({theme}) => theme.hover.border};
-    background-color: ${({theme}) => theme.hover.background};
-    color: ${({theme}) => theme.hover.color};
+    border-color: ${({ theme }) => theme.hover.border};
+    background-color: ${({ theme }) => theme.hover.background};
+    color: ${({ theme }) => theme.hover.color};
   }
-`
+`;
 
 export const PrimaryButton: React.FC<unknown> = (props) => {
-  const {children} = props;
+  const { children } = props;
   const theme: Theme = useTheme();
   const trackButtonClicked = () => {
     console.log(`Primary button is being tracked and its using the theme ${theme.name}`);
-  }
+  };
 
-  return <BaseButton onClick={trackButtonClicked}>{children}</BaseButton>
-}
+  return <BaseButton onClick={trackButtonClicked}>{children}</BaseButton>;
+};
--- packages/egll-lib-react/src/themes/light.ts
+++ packages/egll-lib-react/src/themes/light.ts
@@ -1,15 +1,15 @@
-import { Theme } from '.'
+import { Theme } from '.';
 
 export const LightTheme: Theme = {
   name: 'light',
   base: {
     color: 'white',
     background: '#560bad',
-    border: '#3a0ca3'
+    border: '#3a0ca3',
   },
   hover: {
     color: 'black',
     background: '#3f37c9',
-    border: '#4cc9f0'
-  }
-}
+    border: '#4cc9f0',
+  },
+};
